486cfb6202efa7068e03ebd34ee7d976fab4e53c
shaitr1x <dokoalain17@icloud.com>
2025-11-05 12:12:09 +0100

    Premier commit

---
+++ listelivre.php
@@ -0,0 +1,28 @@
+<?php
+//appel de service
+$xml=file_get_contents("http://troisa.tn/service/books.php");
+//afficher
+$livres= new simplexmlElement($xml);
+//var_dump($livres);
+?>
+<table>
+    <tr>
+        <th>Titre</th>
+        <th>Auteur</th>
+        <th>Prix</th>
+        <th>Key words</th>
+    </tr>
+    <?php
+    foreach($livres->livre as $l)
+    {
+        ?>
+        <tr>
+            <td><?=$l->titre?></td>
+            <td><?=$l->auteur?></td>
+            <td><?=$l->prix?></td>
+            
+        </tr>
+
+    <?php }?>
+    alan franck
+</table>
